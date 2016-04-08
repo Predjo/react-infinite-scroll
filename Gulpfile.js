0bc2b28fc195f5d9f1a11b02043ddbe863ea4aca
Predjo <predovan.nikola@gmail.com>
2016-04-08 12:51:38 +0200

    lib

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@
 var gulp       = require('gulp');
 var del        = require('del');
 var babel      = require('babelify');
+var gulpBabel  = require('gulp-babel');
 var browserify = require('browserify');
 var uglify     = require('gulp-uglify');
 var source     = require('vinyl-source-stream');
@@ -12,7 +13,7 @@ gulp.task('clean', function() {
   del(['./dist/*']);
 });
 
-gulp.task('build', ['clean'], function() {
+gulp.task('build', ['clean', 'buildLib'], function() {
 
   return browserify(['./src/react-infinite-scroll-list.js'])
     .transform(babel.configure({
@@ -23,4 +24,12 @@ gulp.task('build', ['clean'], function() {
     .pipe(source('react-infinite-scroll-list.js'))
     .pipe(streamify(uglify()))
     .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('buildLib', function() {
+  return gulp.src(['./src/react-infinite-scroll-list.js'])
+    .pipe(gulpBabel({
+      presets: ['es2015', 'stage-0']
+    }))
+    .pipe(gulp.dest('lib'));
 });
